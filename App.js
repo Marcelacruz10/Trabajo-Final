e72bd93d8923bf305069eca6f5769bdfadaa93f3
marcelacruz10 <marcelacruz8705@gmail.com>
2024-04-20 23:12:09 -0500

    formularios

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 
 // 2. configurar nuestro servidor
 const app = express ();
-const puerto = 3000;
+const puerto = 9000;
 
 //2.1. configurar  las variables de entorno
 dotenv. config (); 
@@ -15,19 +15,16 @@ dotenv. config ();
 
 //2. establecer la conexión de front
 // obtenemos la ruta absoluta de nuestra carpeta public
-const publicPath = path.join(process.cwd(), "Public");
+const rutaPublica = path.join(process.cwd(), "Public");
 
-app.use(express.static(publicPath));
+app.use(express.static(rutaPublica));
 
 app.use(express.json());
-app.use('/api', usuarioRouter);
 
-app.get('/', (req, res) => {
-    
-  res.sendFile(path.join(publicPath, "index.html"));
+app.get('/', (req,res) => {  
+  res.sendFile(path.join(rutaPublica,"index.html"));
 });
 
-
-app.listen(port, () => {
-    console.log(`El servidor está escuchando en http://localhost:${port}`);
-  });
+app.listen(puerto, () => {
+  console.log(`El servidor está escuchando en http://localhost:${puerto}`);
+})
